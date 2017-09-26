ce0b13b862345141b58ec475798e642d5dc66959
Lenka Vondrackova <lenka.vondr+lenkavon@gmail.com>
2017-09-26 18:01:17 +0200

    practical 02 16:15

---
+++ frontend/src/components/ContactList/ContactListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+export class ContactListItem extends React.Component {
+  render() {
+    const { name, email, id } = this.props.person;
+    return (
+      <div>
+        <Link to={`/contact/${id}`}>{name}</Link>
+        <p>{email}</p>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/pages/ContactDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class ContactDetail extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          I am contact detail page, empty for now :(
+        </h1>
+      </div>
+    )
+  }
+}
