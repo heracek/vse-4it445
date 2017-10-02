1a11bd974e89172b8a7da9a944bb64a1425dac2c
Tomáš Horáček <tomas.horacek@gmail.com>
2017-10-02 19:10:06 +0800

    Downgrade log4js version for PhusionPassenger compatibility

--- backend/src/logging.js
+++ backend/src/logging.js
@@ -2,25 +2,19 @@ import log4js from 'log4js';
 
 export const setupLogging = () => {
   log4js.configure({
-    appenders: {
-      console: { type: 'console' },
-      file: {
+    appenders: [
+      { type: 'console' },
+      {
         type: 'file',
         filename: 'log/app.log',
       },
-    },
-    categories: {
-      default: {
-        appenders: ['file', 'console'],
-        level: 'info',
-      },
-    },
+    ],
     replaceConsole: true,
   });
 
-  const logger = log4js.getLogger('default');
-  console.info = logger.info.bind(logger);
-  console.log = logger.info.bind(logger);
-  console.warn = logger.warn.bind(logger);
-  console.error = logger.error.bind(logger);
+  // const logger = log4js.getLogger('default');
+  // console.info = logger.info.bind(logger);
+  // console.log = logger.info.bind(logger);
+  // console.warn = logger.warn.bind(logger);
+  // console.error = logger.error.bind(logger);
 };
