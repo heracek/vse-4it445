2dd63428ee023014efb238534fe663d1569fd459
Jakub Řičař <jakub.ricar@gmail.com>
2017-10-10 15:04:08 +0200

    Change API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// export const API_URL = 'http://dev.backend.lenkavon.vse.handson.pro';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'http://dev.backend.USERNAME.vse.handson.pro';
+//export const API_URL = 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: API_URL,
